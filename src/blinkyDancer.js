58ccfbfaa0ea07a29a80b4cb2adca7503b97c432
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-18 15:09:39 -0800

    Passes blinkyDancer test specifications

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,7 +2,9 @@ var BlinkyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  var oldStep = blinkyDancer.step;
+  
+
+  // this.oldStep = blinkyDancer.step;
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
@@ -11,9 +13,13 @@ BlinkyDancer.prototype.constructor = BlinkyDancer;
 
 BlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
-  oldStep();
+  //
+  Dancer.prototype.step.call(this);
+  
+
+
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-  blinkyDancer.$node.toggle();
+  this.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -2,7 +2,8 @@
 var Dancer = function(top, left, timeBetweenSteps) {
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this.step();
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.step(timeBetweenSteps);
 
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
@@ -10,10 +11,10 @@ var Dancer = function(top, left, timeBetweenSteps) {
   this.setPosition(top, left);
 };
 
-Dancer.prototype.step = function(timeBetweenSteps) {
+Dancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step.bind(this), timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left) {
