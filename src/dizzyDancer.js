d37fceec04c85a1dbae2c9d780b2e1be132f33bd
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-19 15:27:26 -0800

    Refactors dizzyDancer in ES6 syntax

--- src/dizzyDancer.js
+++ src/dizzyDancer.js
@@ -1,32 +1,28 @@
-var DizzyDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this, top, left, timeBetweenSteps);
+class DizzyDancer extends Dancer {
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
- 
-  this.radius = 50;
-  this.angle = 0;
+  constructor(top, left, timeBetweenSteps) {
+    super(top,left,timeBetweenSteps);  
+    this.radius = 50;
+    this.angle = 0;
+  }
   // this.oldStep = DizzyDancer.step;
-};
-
-DizzyDancer.prototype = Object.create(Dancer.prototype);
-
-DizzyDancer.prototype.constructor = DizzyDancer;
-
-DizzyDancer.prototype.step = function() {
-  // call the old version of step at the beginning of any call to this new version of step
-  //
-  var thiz = this;
-  this.angle += .05;
-  var newX = Math.floor(this.left + this.radius * Math.cos(this.angle));
-  var newY = Math.floor(this.top + this.radius * Math.sin(this.angle));
-  this.$node.css({top: newY, left: newX});
-  this.processID = setTimeout(function(){
-    thiz.step();
-  }, 10);
-  
+  step() {
+    // call the old version of step at the beginning of any call to this new version of step
+    //
+    var thiz = this;
+    this.angle += .05;
+    var newX = Math.floor(this.left + this.radius * Math.cos(this.angle));
+    var newY = Math.floor(this.top + this.radius * Math.sin(this.angle));
+    this.$node.css({top: newY, left: newX});
+    this.processID = setTimeout(function(){
+      thiz.step();
+    }, 10);
+    
+  }
 
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
-};
+}
 
