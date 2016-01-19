df9a2201a96b8fa2775c2c2346e5984b286dc020
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-18 16:46:14 -0800

    Adds Line Up Button with separate class, adds all new dancers to dancers array

--- src/init.js
+++ src/init.js
@@ -28,6 +28,11 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+  });
+
+  $(".lineUpButton").on("click", function() {
+    //Line up dancers in dancersArray
   });
 });
 
