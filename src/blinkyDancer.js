3e0316c53be352247a25d9dfec1a176ea1d00bf7
orlandoc01 <orlandoc@sas.upenn.edu>
2016-01-19 15:20:09 -0800

    Refactors BlinkyDancer class using ES6 syntax

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,25 +1,13 @@
-var BlinkyDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this, top, left, timeBetweenSteps);
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-  
-
-  // this.oldStep = blinkyDancer.step;
-};
-
-BlinkyDancer.prototype = Object.create(Dancer.prototype);
-
-BlinkyDancer.prototype.constructor = BlinkyDancer;
-
-BlinkyDancer.prototype.step = function() {
-  // call the old version of step at the beginning of any call to this new version of step
-  //
-  Dancer.prototype.step.call(this);
-  
+class BlinkyDancer extends Dancer {
+  step() {
+    // call the old version of step at the beginning of any call to this new version of step
+    super.step();
+    
 
 
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  this.$node.toggle();
-};
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    this.$node.toggle();
+  }
+}
